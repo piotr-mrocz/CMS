f0ac5ea31a85a5c7bf08136dd210bf2212cc4fbd
Piotr Mroczkowski <p.mrocz@vp.pl>
2024-02-07 10:30:26 +0100

    create function to comunicate with strapi

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+
+interface Restaurant {
+  name: string;
+  description: string;
+}
+
+interface Entry<T> {
+  id: number;
+  attributes: T;
+}
+
+interface Response {
+  data: Entry<Restaurant>[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit
+{
   title = 'angular-app';
+
+  error: any | undefined;
+  restaurants$: Observable<Restaurant[]> | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void
+  {
+    const url = "http://localhost:1337/api/restaurants";
+    const opts = { params: { populate: "*" } };
+
+    this.restaurants$ = this.http.get<Response>(url, opts).pipe(
+      catchError((error) => this.handleError(error)),
+      map((response) => response.data.map((x) => x.attributes))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never>
+  {
+    this.error = error;
+    return of();
+  }
 }
